cd96e3e58db4b37119c5736dfd89914378261314
BLCheung <925306022@qq.com>
2019-10-09 01:27:46 +0800

    Project initial

---
+++ vue.config.js
@@ -0,0 +1,22 @@
+// 拿到路由路径变量
+const path = require('path');
+
+module.exports = {
+  configureWebpack: (config) => {
+    if (process.env.NODE_ENV === 'production') {
+      // 为生产环境修改配置...
+    } else {
+      // 为开发环境修改配置...
+      return {
+        resolve: {
+          alias: {
+            '@js': path.resolve(__dirname, './src/assets/js'),
+            '@css': path.resolve(__dirname, './src/assets/css'),
+            '@img': path.resolve(__dirname, './src/assets/img'),
+            '@cpm': path.resolve(__dirname, './src/components')
+          }
+        }
+      };
+    }
+  }
+};
